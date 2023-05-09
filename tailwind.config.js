afd3f9c9060efbb49d8299fb9c666b7a7ca53e81
Joe <joefaridcastillo@gmail.com>
2023-05-09 19:51:35 -0400

    worked on nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: [
-    './pages/**/*.{html,jsx,js}',
-    './components/**/*.{html,jsx,js}',
-  ],
+  content: ["./pages/**/*.{html,jsx,js}", "./components/**/*.{html,jsx,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-  images: [
-    "image.tmbd.org",
-
-]
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+  images: ["image.tmbd.org"],
+};
